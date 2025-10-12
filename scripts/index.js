7a827d150ac6d97a77c904fc09073814fed6f0d3
Asadulla Khatami <asadullakhatami@gmail.com>
2025-10-12 21:47:37 +0600

    responsive nav bar design js code are written

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,15 @@
-// console.log("hello world");
+// console.log("hello word");
 
+const menuBar = document.querySelector('#menuBar');
+const bar = document.querySelector('#bar');
+const openNav = document.getElementById('open-nav');
+
+
+menuBar.addEventListener('click', () => {
+
+    if (openNav.style.display === 'flex') {
+        openNav.style.display = 'none';
+    }else{
+        openNav.style.display = 'flex';
+    }
+});
