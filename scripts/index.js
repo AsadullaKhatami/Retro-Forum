372835659b734abbb48c854920d1f41132258a96
Asadulla Khatami <asadullakhatami@gmail.com>
2025-10-12 23:30:03 +0600

    api response checked

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,28 @@
 const menuBar = document.querySelector('#menuBar');
 const bar = document.querySelector('#bar');
 const openNav = document.getElementById('open-nav');
+const url = 'https://openapi.programming-hero.com/api/retro-forum/latest-posts'
 
+// backend api link latest posts
+async function loadData(url){
+    try{
+        let response = await fetch(url);
+        let data = await response.json();
+        latestPost(data);
+    }catch(err){
+        console.error("Error fetching data:", err);
+    }
+}
+
+// latest post 
+// const latestPost = fetchData('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+
+function latestPost(data) {
+    console.log(data[0].author.name);
+}
+
+
+// console.log(latestPost)
 
 menuBar.addEventListener('click', () => {
 
@@ -13,3 +34,5 @@ menuBar.addEventListener('click', () => {
         openNav.style.display = 'flex';
     }
 });
+
+loadData(url);
